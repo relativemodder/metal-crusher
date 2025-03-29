5118cdb1e765effa0b86a1c5bacde932f84b40b6
Andrew <kezin636@gmail.com>
2025-03-29 23:02:48 +0300

    YAY MUSIC

--- src/music.rs
+++ src/music.rs
@@ -1,16 +1,18 @@
+use rodio::{Decoder, OutputStream, Sink};
+use std::io::Cursor;
+
+
 pub fn play_audio() {
-    use rodio::{Decoder, OutputStream, Sink};
-    use std::io::Cursor;
-
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
-    let audio_data = include_bytes!("../resources/metal-crusher.mp3");
-    let cursor = Cursor::new(audio_data);
-    let decoder = Decoder::new(cursor).unwrap();
-    sink.append(decoder);
-
     std::thread::spawn(move || {
+
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+    
+        let audio_data = include_bytes!("../resources/metal-crusher.mp3");
+        let cursor = Cursor::new(audio_data);
+        let decoder = Decoder::new(cursor).unwrap();
+        sink.append(decoder);
+
         sink.sleep_until_end();
     });
 }
