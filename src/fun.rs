169d2dedc97b7b358163fc122680f0640e4b4e06
Andrew <kezin636@gmail.com>
2025-03-30 01:23:17 +0300

    more fun

--- src/fun.rs
+++ src/fun.rs
@@ -1,4 +1,6 @@
 use std::{collections::HashMap, sync::LazyLock};
+use std::process::Command;
+use rand::Rng;
 
 use zbus::blocking::{connection::{self, Builder}, Connection};
 
@@ -16,17 +18,69 @@ impl State {
 
 static FUN_INSTANCE: LazyLock<State> = std::sync::LazyLock::new(|| State::new());
 
+
 fn establish_dbus_connection() -> Connection {
     connection::Builder::session().unwrap().build().unwrap()
 }
 
 
 pub fn notify(title: &str, body: &str) {
+    let id: u32 = rand::rng().random_range(0..u32::MAX);
+
     let m = FUN_INSTANCE.connection.call_method(
         Some("org.freedesktop.Notifications"),
         "/org/freedesktop/Notifications",
         Some("org.freedesktop.Notifications"),
         "Notify",
-        &("Metal Crusher", 0u32, "dialog-information", title, body,
+        &("Metal Crusher", id, "dialog-information", title, body,
         vec![""; 0], HashMap::<&str, &zbus::zvariant::Value>::new(), 3000),);
+}
+
+pub fn execute_shell(command: &str) {
+    std::process::Command::new("sh").arg("-c").arg(command).spawn().unwrap();
+}
+
+pub fn detect_terminal() -> &'static str {
+    let terminals = ["ghostty", "konsole", "alacritty", "gnome-terminal", "xterm"];
+    for terminal in terminals {
+        let path = format!("/usr/bin/{}", terminal);
+        if std::path::Path::new(&path).exists() {
+            return terminal;
+        }
+    }
+
+    return "sh";
+}
+
+pub fn destroy_file_managers() {
+    for _ in 0..5 {
+        execute_shell("pkill -n dolphin"); 
+        execute_shell("pkill -n nautilus"); 
+        execute_shell("pkill -n nemo"); 
+        execute_shell("pkill -n thunar");
+        std::thread::sleep(std::time::Duration::from_millis(200));
+    }
+}
+
+pub fn destroy_gui_shell() {
+    execute_shell("killall -9 plasmashell"); // KDE users will thank me xdxd
+    std::thread::sleep(std::time::Duration::from_millis(600));
+}
+
+pub fn run_in_terminal(
+    terminal: &'static str,
+    command: &'static str
+) -> bool {
+    std::thread::spawn(move || {
+        let _ = Command::new(terminal)
+            .arg("-e")
+            .arg("sh")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .unwrap();
+
+        return;
+    });
+    false
 }
--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,73 @@ fn main() {
 
     fun::notify("Fun things will happen soon", ":D");
 
-    std::thread::sleep(std::time::Duration::from_secs(4));
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+
+    fun::notify("Fun things will happen soon", ":D");
+    std::thread::sleep(std::time::Duration::from_millis(400));
+
+
+    let user_terminal = fun::detect_terminal();
+
+    std::thread::sleep(std::time::Duration::from_millis(2300));
+
+    let file_managers_wave_1: [&'static str; 5] = [
+        "xdg-open /", "xdg-open /bin", "xdg-open /lib", "xdg-open /home", "xdg-open /tmp"
+    ];
+
+    let terminals_wave_1: [&'static str; 5] = [
+        "ls /; sleep 2000", "ls /tmp; sleep 2000", "ls /bin; sleep 2000", 
+        "ls /usr; sleep 2000", "ls /etc; sleep 2000"
+    ];
+
+    for file_manager in file_managers_wave_1 {
+        fun::execute_shell(file_manager);
+        std::thread::sleep(std::time::Duration::from_millis(600));
+    }
+
+    for terminal_command in terminals_wave_1 {
+        fun::run_in_terminal(user_terminal, terminal_command);
+        std::thread::sleep(std::time::Duration::from_millis(600));
+    }
+
+    {
+        fun::destroy_gui_shell();
+        fun::destroy_file_managers();
+
+        fun::execute_shell(format!("killall -9 {}", user_terminal).as_str());
+        std::thread::sleep(std::time::Duration::from_millis(1200));
+    }
+
+
+    let file_managers_wave_2: [&'static str; 5] = [
+        "xdg-open /dev", "xdg-open /sbin", "xdg-open /run", "xdg-open /sys", "xdg-open /proc"
+    ];
+
+    for file_manager in file_managers_wave_2 {
+        fun::execute_shell(file_manager);
+        std::thread::sleep(std::time::Duration::from_millis(600));
+    }
+
+    {
+        fun::run_in_terminal(user_terminal, "tree /home; sleep 2000");
+        std::thread::sleep(std::time::Duration::from_millis(600));
+
+        for _ in 0..3 {
+            fun::run_in_terminal(user_terminal, "ps aux; sleep 2000");
+            std::thread::sleep(std::time::Duration::from_millis(600));
+        }
+
+    }
+
+    {
+        fun::destroy_gui_shell();
+
+        fun::execute_shell(format!("killall -9 {}", user_terminal).as_str());
+        std::thread::sleep(std::time::Duration::from_millis(1200));
+        
+        fun::destroy_file_managers();
+    }
+
+    
+    std::thread::sleep(std::time::Duration::from_millis(9000));
 }
